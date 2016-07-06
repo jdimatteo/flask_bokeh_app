8a276f39f7f4703a2105d0f1707944d11c3456ae
John DiMatteo <jdimatteo@boulderlabs.com>
2016-07-06 13:36:17 -0600

    Fixed based on @canavandl's code provided on https://gitter.im/bokeh/bokeh

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from bokeh.client import push_session
 from bokeh.embed import autoload_server
 from bokeh.plotting import figure, curdoc
-
+from bokeh.document import Document
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # figure() function auto-adds the figure to curdoc()
     plot = figure()
     plot.circle([1,2], [3,4])
+    document = Document()
+    document.add_root(plot)
 
-    session = push_session(curdoc())
-    script = autoload_server(plot, session_id=session.id)
+    session = push_session(document)
+    script = autoload_server(None, session_id=session.id)
+
     return render_template('index.html', bokeh_script=script)
