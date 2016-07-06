a5fc614a2d814fac421a14e549b1a451f6df3924
John DiMatteo <jdimatteo@boulderlabs.com>
2016-07-06 12:10:00 -0600

    Reproduced could not find session error

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from bokeh.client import push_session
 from bokeh.embed import autoload_server
+from bokeh.plotting import figure, curdoc
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    script = autoload_server(model=None, app_path="/sliders")
+    # figure() function auto-adds the figure to curdoc()
+    plot = figure()
+    plot.circle([1,2], [3,4])
+
+    session = push_session(curdoc())
+    script = autoload_server(plot, session_id=session.id)
     return render_template('index.html', bokeh_script=script)
